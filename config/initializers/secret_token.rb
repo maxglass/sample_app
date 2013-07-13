e08f7614a7958bfbfe5b6d95eeca16ed0a153a12
MaxDev <maxdev@Max-Glass-Mac-4.local>
2013-07-13 18:24:44 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '7ef15d369b8cb486554f0f572bb3cc97debf93e1ad612c2d46d3a17f74c4dae827b23a20abda5ee3cb4086b4a1268aa3eba83eee335cf7e5f7155189fb23a7b4'
